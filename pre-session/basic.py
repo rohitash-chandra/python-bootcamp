8248f16aa5b76b811c01d4f3c073adbb7b597d44
RC <c.rohitash@gmail.com>
2021-08-25 17:49:10 +1000

    basic

---
+++ pre-session/basic.py
@@ -0,0 +1,23 @@
+number = 6677
+ 
+number_decimal = 18.6 #cpp or java double or float
+
+height = 1.8
+
+name = ' Rohitash Chandra'
+
+print(number)
+
+print(number, ' is number')
+
+
+print(number_decimal, ' is decimal ')
+
+
+print(height,'               is height ')
+
+print(name)
+
+
+print(name, ' is my name')
+
