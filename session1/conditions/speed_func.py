570e52317fd1ab86bba26aac7e2c9118dc1e22dc
RC <c.rohitash@gmail.com>
2021-08-27 11:33:54 +1000

    speed pro has problem

--- session1/conditions/speed_func.py
+++ session1/conditions/speed_func.py
@@ -6,25 +6,40 @@ def speed_function_simple(speed): # example of function with no return value
         print(" you. have to pay a fine")
 
 
-def speed_function_advanced(speed): # example of function with return value
+def speed_function_advanced(speed, provisonal): # example of function with return value
 
     total_fine = 0
+
+    print(provisonal, ' is provisional')
         
     if (speed < 80):
         print(" speed is ok.")
 
-    elif (speed >= 80) and (speed < 100):
+    elif ((speed >= 80) and (speed < 100)) and (provisonal==True):
+        base_fine = 500
+        speed_diff = speed - 80
+        extra_fine = speed_diff * 10
+        total_fine = extra_fine + base_fine 
+        print(" fine of (Pro is True) ", total_fine)
+
+    
+    elif ((speed >= 80) and (speed < 100)) and (provisonal==False):
         base_fine = 200
         speed_diff = speed - 80
         extra_fine = speed_diff * 10
         total_fine = extra_fine + base_fine 
         print(" fine of  ", total_fine)
+
+
     elif (speed >= 100) and (speed < 140):
         print(" fine of 600 AUD")
     else:
         print(" time to go to prison. ")
 
     return total_fine
+
+
+# adap it for fine system in your country of residence
 
  
 
@@ -39,7 +54,12 @@ def main():
 
     speed_adv = int(input("Enter speed in km/hr for advanced function:  "))
 
-    fine = speed_function_advanced(speed_adv)
+
+    provisional = bool(input("Are you having a provisional licence? "))
+    
+
+
+    fine = speed_function_advanced(speed_adv, provisional)
 
     print('the fine returned to main is: ', fine, '  dollars')
 
