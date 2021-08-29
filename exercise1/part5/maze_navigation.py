02a1b2f39dc9ca6fcbac03bf3f4be229c023d024
KarlosTan <kelvin.tang@outlook.com.au>
2021-08-29 23:02:24 +1000

    29/8

--- exercise1/part5/maze_navigation.py
+++ exercise1/part5/maze_navigation.py
@@ -1,30 +1,51 @@
-'''FIND-PATH(x, y) 
-    1. 	if (x,y outside maze) return false 
-    2. 	if (x,y is goal) return true 
-    3. 	if (x,y not open) return false 
-    4. 	mark x,y as part of solution path 
-    5. 	if (FIND-PATH(North of x,y) == true) return true 
-    6. 	if (FIND-PATH(East of x,y) == true) return true 
-    7. 	if (FIND-PATH(South of x,y) == true) return true 
-    8. 	if (FIND-PATH(West of x,y) == true) return true 
-    9. 	unmark x,y as part of solution path 
-    10. return false '''
+import csv
+
+with open('Maze.csv') as csvfile:
+    Maze = list(csv.reader(csvfile, delimiter = ','))
+
+#Duplicate Maze for Solutions
+Sol = Maze
+
+# O represents obstacle
+# U represents unchecked, open cell
+# C represents checked path
+# S represents start
+# G represents goal
 
 
 def find_path(x, y):
-    if ( ):
-        return False 
-    if ( ):
-        return False 
+    if Maze[x][y] == 'O':
+        return False
+    if Maze[x][y] == 'C':
+        return False
+    if Maze[x][y] == 'G':
+        print(x,y)
+        return True
+    Maze[x][y] = 'C'
+    if find_path(x+1, y):       #prioritises down-up search before right-left
+        print(x,y)
+        Sol[x][y] = 'A'
+        return True
+    if find_path(x-1, y):
+        print(x,y)
+        Sol[x][y] = 'A'
+        return True
+    if find_path(x, y+1):
+        print(x,y)
+        Sol[x][y] = 'A'
+        return True
+    if find_path(x, y-1):
+        print(x,y)
+        Sol[x][y] = 'A'
+        return True
 
+for r in range(len(Maze)):
+    if 'S' in Maze[r]:
+        startx = r
+        starty = Maze[r].index('S')
 
+find_path(startx, starty)
 
-
-
-
-
-#-------------------
-
-
-
-    
+with open('Solution.csv', 'w', newline = '') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(Sol)
