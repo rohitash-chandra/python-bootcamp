8c8202ee6b659b78d9da2d47b3efe72723fdee7e
HakiimJ <ahmadhakiimjamaluddin@gmail.com>
2021-08-30 21:28:12 +0800

    edit

--- pre-session/basic.py
+++ pre-session/basic.py
@@ -1,44 +1,10 @@
-number = 123  # integer 
- 
-number_decimal = .123 #cpp or java double or float
-
-height = 1.73
-
-name = 'Hakiim Jamaluddin'  # string
-
-print(number)
-print(number, 'is number')
-print(number_decimal, ' is decimal ')
-print(height,'is height ')
-print(name)
-print(name,'is my name')
-
-print('lets calculate')
-
-
-radius = 12.25
-pi = 3.14
-
-area_circle = pi * radius * radius 
-
-print(area_circle, ' is area of the circle')
-
-
-# triangle (ask the user for input)
-
-
-print('Good day mate. This program claculates area of triangle')
-
-print('please enter the base: ')
-base = input()
-
-print('please enter the height: ')
+print('Your name?')
+name = input()
+print('Your height?')
 height = input()
+print('Your weight?')
+weight = input()
 
-print(base, height,  ' are the base and height, respectively')
-
-area_triangle = 0.5 * int(base) * int(height)
-
-
-print(area_triangle,  ' is the area of the triangle')
-
+print('Hi ',name,'.')
+print('Your height is ',height,' and weight is ',weight,'.')
+print('You are MF fat!')
