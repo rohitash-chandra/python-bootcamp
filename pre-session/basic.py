6a47d05d7ee9d9cd97afee895cbcbfbbfdf53327
HakiimJ <ahmadhakiimjamaluddin@gmail.com>
2021-08-30 21:48:15 +0800

    edit

--- pre-session/basic.py
+++ pre-session/basic.py
@@ -1,10 +1,24 @@
 print('Your name?')
 name = input()
-print('Your height?')
+print('Your height (m)?')
 height = input()
-print('Your weight?')
+print('Your weight (kg)?')
 weight = input()
+
+def bmi (h,w):
+    bmi = w/(h*h)
+    if bmi < 18.5: 
+        bmi_status = 'underweight'
+    elif bmi >= 18.5 and bmi < 25: 
+            bmi_status = 'normal'
+    elif bmi >= 25 and bmi < 30: 
+            bmi_status = 'overweight'
+    elif bmi >= 30: 
+            bmi_status = 'obese'
+    return(bmi, bmi_status)
+
+BMI, BMI_status = bmi(float(height), float(weight))
 
 print('Hi ',name,'.')
 print('Your height is ',height,' and weight is ',weight,'.')
-print('You are MF fat!')
+print('Your BMI is ', BMI, ' and you are ', BMI_status,'.')
