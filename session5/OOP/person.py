dc93c8a86b51baa1aea74967da507aea4264fd2e
RC <c.rohitash@gmail.com>
2021-09-03 11:12:28 +1000

    begin OOP

--- session5/OOP/person.py
+++ session5/OOP/person.py
@@ -3,22 +3,29 @@
 
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, w):
         self.name = name
         self.age = age
+        self.weight = w
         # add others
 
     def print_infor(self):
         print(self.name, ' name')
         print(self.age, ' is age ')
+        print(self.weight, ' is weight ')
 
 
 
 if __name__ == "__main__":
-    p = Person("ranjeeta", 23)
-    print(p.name)
 
-    r = Person("rohitash chandra", 37)
-    #print(r.name)
 
+    p = Person("ranjeeta kumari", 40, 55)
+
+    r = Person("rohitash chandra", 37, 78)
+
+    # define yours 
+
+    
     r.print_infor()
+
+    p.print_infor()
